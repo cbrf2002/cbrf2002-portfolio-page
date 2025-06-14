c0420299cd64bd894636b584421c54ba536bb46c
cbrf2002 <cbrf2002@proton.me>
2025-06-14 11:51:49 +0800

    Configure image settings in Next.js for optimized loading and caching

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60 * 60 * 24 * 30,
+    
+    loader: 'custom',
+    loaderFile: './src/lib/image-loader.ts',
+  },
 };
 
 export default nextConfig;
